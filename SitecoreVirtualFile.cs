8d88549cdeeae6b55bee1ea10a5378a6ddf7d85d
Gobiner <Gobiner@users.noreply.github.com>
2016-08-13 17:34:13 -0700

    copy existing code over fork

---
+++ SitecoreVirtualFile.cs
@@ -0,0 +1,51 @@
+﻿using ImageResizer.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+using System.Web;
+
+namespace ImageResizer.Sitecore.Plugin
+{
+    public class SitecoreVirtualFile : IVirtualFile
+    {
+        protected ResizeSettings query;
+        protected HttpRequest request;
+
+        public SitecoreVirtualFile(string virtualPath, NameValueCollection query, HttpRequest request)
+        {
+            this.request = request;
+            this._virtualPath = virtualPath;
+            this.query = new ResizeSettings(query);
+        }
+
+        public System.IO.Stream Open()
+        {
+            var tmp = global::Sitecore.Resources.Media.MediaManager.ParseMediaRequest(request);
+            if (tmp != null)
+            {
+                MediaItem item = tmp.MediaUri.Database.GetItem(tmp.MediaUri.MediaPath, tmp.MediaUri.Language, tmp.MediaUri.Version);
+
+                return item.GetMediaStream();
+            }
+
+            DynamicLink dynamicLink;
+            if (!DynamicLink.TryParse(VirtualPath, out dynamicLink))
+                throw new ApplicationException("VirtualImageProviderPlugin : cannot parse virtual path: " + VirtualPath);
+
+            MediaItem mediaItem = global::Sitecore.Context.Database.GetItem(dynamicLink.ItemId, dynamicLink.Language ?? global::Sitecore.Context.Language);
+
+            return mediaItem.GetMediaStream();
+        }
+
+        protected string _virtualPath;
+        public string VirtualPath
+        {
+            get { return _virtualPath; }
+        }
+    }
+}
---
+++ SitecoreVirtualImageProviderPlugin.cs
@@ -0,0 +1,54 @@
+﻿using ImageResizer.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Links;
+using System.Web;
+
+namespace ImageResizer.Sitecore.Plugin
+{
+    public class SitecoreVirtualImageProviderPlugin : IPlugin, IVirtualImageProvider, IFileExtensionPlugin
+    {
+        public IPlugin Install(global::ImageResizer.Configuration.Config c)
+        {
+            c.Plugins.add_plugin(this);
+            return this;
+        }
+
+        public bool Uninstall(global::ImageResizer.Configuration.Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+
+        private string FixVirtualPath(string virtualPath)
+        {
+            return virtualPath.Substring(virtualPath.LastIndexOf("~")); // LOL it's the only way, I'm so sorry but it's true.
+        }
+
+        public bool FileExists(string virtualPath, NameValueCollection queryString)
+        {
+            var tmp = global::Sitecore.Resources.Media.MediaManager.ParseMediaRequest(HttpContext.Current.Request);
+            
+            virtualPath = FixVirtualPath(virtualPath);
+            
+            DynamicLink dynamicLink;
+            return tmp != null || DynamicLink.TryParse(virtualPath, out dynamicLink);
+        }
+
+        public IVirtualFile GetFile(string virtualPath, NameValueCollection queryString)
+        {
+            virtualPath = FixVirtualPath(virtualPath);
+
+            return new SitecoreVirtualFile(virtualPath, queryString, HttpContext.Current.Request);
+        }
+
+        public IEnumerable<string> GetSupportedFileExtensions()
+        {
+            return new string[] { "ashx" };
+        }
+    }
+}
